4ac8f2aa76b02a87b13c5a2261a4bda17f4a0e83
Start Coding Student 2 <session2@START-Coding-MBP-12.local>
2016-04-28 16:54:30 -0700

    Nav Bar

---
+++ Business Side/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>Nav toggler</title>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+
+  <nav>
+    <div class="navToggle">
+      <div class="icon"></div>
+    </div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Contact</a></li>
+   </ul>
+  </nav>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+        <script src="index.js"></script>
+
+  <div id="test" style="width: 100px; height: 100px; background-color: aqua; margin-left: 25%;">
+    <p>Test</p>
+  </div>
+  </body>
+</html>
