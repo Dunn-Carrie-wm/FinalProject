8381bfc1a805b1c6bd40a7b0c60602891fb862c7
Start Coding Student 2 <session2@START-Coding-MBP-12.local>
2016-05-05 16:19:20 -0700

    update

---
+++ Business Side/note.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Note Pad</title>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="note.css">
+    <script src="note.js"></script>
+</head>
+<body>
+
+<div style="background-color: #00b7bb; height: 700px; width: 370px; position: absolute;">
+
+</div>
+
+<div style="background-color: red; height: 700px; width: 900px; margin-left: 400px; position: absolute; ">
+    <form>
+
+    </form>
+</div>
+
+
+</body>
+</html>
