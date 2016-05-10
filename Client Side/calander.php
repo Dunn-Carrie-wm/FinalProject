21c3ae797b5904a36bc3d5401d46867cb5f958a2
Juan Chavez <juan.chavez@west-mec.org>
2016-05-10 16:42:46 -0700

    xD

--- Client Side/calander.php
+++ Client Side/calander.php
@@ -7,53 +7,77 @@
       <link rel='stylesheet prefetch' href='http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css'>
       <link rel="stylesheet" href="cal.css">
       <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+
+      <style>
+        .close
+        {
+          float: right;
+          width: 70px;
+          height: 55px;
+          background: transparent no-repeat;
+          border: none;
+          cursor: pointer;
+          overflow: hidden;
+          outline: none;
+          font-size: 175%;
+        }
+        #secondDiv
+        {
+          position: absolute;
+          margin-left: auto;
+          margin-right: auto;
+          width: 480px;
+          height: 580px;
+          z-index: 15;
+        }
+        .information
+        {
+          margin-left: auto;
+          margin-right: auto;
+          margin-top: 5.5%;
+          border: solid;
+          z-index: 3;
+          background: rgba(255, 255, 255, 1);
+          position: relative;
+        }
+      </style>
   </head>
 
   <body>
-    <div id="calendar">
-      <div id="calendar_header">
-        <i class="icon-chevron-left"></i>
-        <h1></h1>
-        <i class="icon-chevron-right"></i>
-      </div>
-
-      <div id="calendar_weekdays"></div>
-      <div id="calendar_content"></div>
-    </div>
-
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="cal.js"></script>
 
     <script>
       $(document).ready(function(){
-        var toggle = 0;
         $(".today").click(function(){
-          if(toggle == 0)
-          {
-            $('#calendar').hide();
-            $('#result').show();
-            toggle++;
-          }
-          else
-          {
-            $('#calendar').show();
-            $('#result').hide();
-            toggle--;
-          }
+          $('#secondDiv').show();
+        });
+
+        $(".close").click(function(){
+          $('#secondDiv').hide();
         });
       });
     </script>
 
-    <div id="result" style="height: 700px; width: 510px; margin-left: 400px; position: absolute; display: none; background-color: black">
-      <br>
-      <div style="background-color: pink;">
-      <h2 id="header" style="text-decoration: underline; font-size: 50px; text-align: center; font-family: Times New Roman; color: black">Test</h2>
+    <div id="calendar">
+      <div id="calendar_header">
+        <h1></h1>
       </div>
 
-      <div id="text" style=" height: 400px; width: 900px; text-align: center; font-size: 30px; border: dashed; border-color: black">
+      <div id="calendar_weekdays"></div>
+      <div id="calendar_content"></div>
 
+      <div id="secondDiv" style="display: none; background-color: black">
+        <div style="background-color: pink;">
+          <h2 id="header" style="text-decoration: underline; font-size: 50px; text-align: center; font-family: Times New Roman; color: black">Test</h2>
+        </div>
+
+        <div class="information" style="height: 55%;">
+          <div>
+            <button class="close">&#10006;</button>
+          </div>
+        </div>
       </div>
     </div>
-
   </body>
 </html>
