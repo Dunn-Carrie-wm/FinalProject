15aa50eb26e02f130d0dac490b28354f00207925
Start Coding Student 2 <session2@START-Coding-MBP-12.local>
2016-05-12 16:12:25 -0700

    Nav fixes

--- Client Side/login.php
+++ Client Side/login.php
@@ -66,30 +66,6 @@
     }
 ?>
 
-    <nav>
-        <div class="navToggle">
-            <div class="icon"></div>
-        </div>
-        <ul>
-            <?php
-            if(isset($_SESSION['user_id']))
-                echo "<li><a href=\"profile2.php\">Profile</a></li>";
-            else
-                echo "<li><a href=\"login.php\">Profile</a></li>";
-            ?>
-            <li><a href="noteinput.php">Note Pad</a></li>
-            <li><a href="reminderinput.php">Reminder</a></li>
-            <li><a href="general.php">General Facts</a></li>
-
-            <?php
-            if(isset($_SESSION['user_id']))
-                echo "<li><a href=\"logout.php\">Log Out</a></li>";
-            else
-                echo "<li><a href=\"login.php\">Log In</a></li>";
-            ?>
-        </ul>
-    </nav>
-
     <script>
         $(".navToggle").click (function(){
             $(this).toggleClass("open");
